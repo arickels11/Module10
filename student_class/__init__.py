22f8271d60fb70e4a52b86128dd11d3baafb124f
arickels11 <arickels93@gmail.com>
2019-10-29 15:25:49 -0500

    topic 3 assignment test 4 pass

--- student_class/__init__.py
+++ student_class/__init__.py
@@ -4,11 +4,10 @@ Alex Rickels"""
 
 class Student:
     """Student class"""
-    def __init__(self, lname: object, fname: object, major: object, gpa: object = 0.0) -> object:
-        """
-
-        :rtype: object
-        """
+    def __init__(self, lname, fname, major, gpa=0.0):
+        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
+        if not name_characters.issuperset(lname):
+            raise ValueError
         self.last_name = lname
         self.first_name = fname
         self.major = major
